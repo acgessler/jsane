f2ed0f8f238a79ba0341b2edfbb1410a1d022298
acgessler <alexander.gessler@gmx.net>
2014-08-17 14:25:07 -0700

    Test case really executes instrumented file now.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 
+// Module includes
 var	chai = require('chai')
 ,	fs = require('fs')
 ,	path = require('path')
-,	esnull = require('../src/esnull-instrument')
+,	esnull = require('../src/jsane-instrument')
 ;
+
+// Shortcuts to common chai functions
+var expect = chai.expect;
+
 
 /** Instrument and run ./testcase/test<id>.js and verify that all
     warnings and errors occur as defined in testcase comments. 
@@ -15,8 +20,11 @@ function runTestcase(id) {
 	var	src_file = path.join(__dirname, 'testcase', 'test' + id + '.js')
 	,	source = fs.readFileSync(src_file, {encoding : 'utf-8'})
 	;
-	source = esnull.instrumentCode(source);
-	eval(source);
+	source = esnull.instrumentCode(source).toString();
+	expect(source).to.be.a('string');
+	expect(function() {
+		eval(source);
+	}).not.to.throw();
 	console.log(source);
 }
 
