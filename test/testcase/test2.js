0b831f44d9c471ef0409c39dc3127b66ab1750b9
acgessler <alexander.gessler@gmx.net>
2014-08-26 23:54:37 -0700

    Proper E2 testcase.

--- test/testcase/test2.js
+++ test/testcase/test2.js
@@ -2,6 +2,7 @@
 
 var f = function() { return this; };
 var f_strict = function() { "use strict"; return this; };
+var get_f_strict_name = function() { return "f_strict"; };
 var b = null;
 
 var o = {};
@@ -12,23 +13,39 @@ o.o = {};
 o.o.f = f;
 o.o.f_strict = f_strict;
 
-
+// Verify that functions are invoked with the correct |this|
 "<<[expect=]";
 var c0 = f();
-// JSane: off
-//expect(c0).to.be(global);
+// JSane: ignore
+expect(c0).to.equal(global);
 
 var c1 = f_strict();
-// JSane: off
-//expect(c1).to.be(undefined);
+// JSane: ignore
+expect(c1).to.equal(undefined);
 
 var c2 = o.f();
-// JSane: off
-//expect(c2).to.be(o);
+// JSane: ignore
+expect(c2).to.equal(o);
 
-var c3 = f_strict();
-// JSane: off
-//expect(c3).to.be(o);
+var c3 = o.f_strict();
+// JSane: ignore
+expect(c3).to.equal(o);
+
+var c4 = o.o.f();
+// JSane: ignore
+expect(c4).to.equal(o.o);
+
+var c5 = o.o.f_strict();
+// JSane: ignore
+expect(c5).to.equal(o.o);
+
+var c6 = o.o["f"]();
+// JSane: ignore
+expect(c6).to.equal(o.o);
+
+var c7 = o.o[get_f_strict_name()]();
+// JSane: ignore
+expect(c7).to.equal(o.o);
 ">>";
 
 "<<[expect=E2]";
