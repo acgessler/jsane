1b13f4b0aa7db30c9be69a2d06ff910d9ea8be0b
Alexander Gessler <alexander.gessler@gmx.net>
2014-10-22 17:27:37 +0200

    fix tests after refactoring.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 //
 // This file is typically require()d in two different scenarios:
 //   - During build (i.e. from grunt) to use JSane's API to instrument code.
-//   - At runtime by jSane-generated code to load its own runtime library
+//   - At runtime by jSane generated code to load its support runtime library
+//     (only if configured to use the regular module system as opposed
+//      to directly embed the runtime source)
 //
 
 // Make the runtime library available as |runtime|. If this name
-// changes, src/instrument.js needs to be updated.
-exports.runtime = require('./src/runtime/runtime');
+// changes, src/instrumentation/instrument.js needs to be updated.
+exports.runtime = require('./compiled/runtime.min');
 
-// Public API to instrument files
-exports.processFile = require('./src/instrumentation/standalone').processFile;
+// Public API to instrument files. This is loaded lazily.
+exports.processFile = function() {
+	return require('./src/instrumentation/standalone').processFile.apply(this, arguments);
+}
