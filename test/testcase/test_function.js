6b424838f507493b0ccea1ff1f8f1b6309df21e6
Alexander Gessler <alexander.gessler@gmx.net>
2014-10-28 11:08:22 +0100

    Update Function.toString() tests.

--- test/testcase/test_function.js
+++ test/testcase/test_function.js
@@ -1,13 +1,23 @@
 // Test to verify Function class (i.e. toString) behaviour is unchanged
 
 function foo(a) {console.log(1); }
+var bar = function(a, b) {console.log(2); }
+var baz = function baz() {console.log(3); }
 
 var foo_string = "function foo(a) {console.log(1); }";
+var bar_string = "function(a) {console.log(2); }";
+var baz_string = "function baz(a) {console.log(3); }";
 
 "<<[expect=]";
 
 expect(foo.toString()).to.equal(foo_string);
 expect(foo.length).to.equal(1);
+
+expect(bar.toString()).to.equal(bar_string);
+expect(bar.length).to.equal(2);
+
+expect(baz.toString()).to.equal(baz_string);
+expect(baz.length).to.equal(0);
 
 ">>"
 
